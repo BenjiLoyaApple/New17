7cb14abe12985ad60f07fbea4e068d0d8035c2f5
Benji Loya <benjiloya.apple@gmail.com>
2024-02-01 20:56:08 +0500

    Initial Commit

---
+++ New17/New17App.swift
@@ -0,0 +1,17 @@
+//
+//  New17App.swift
+//  New17
+//
+//  Created by Benji Loya on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct New17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
